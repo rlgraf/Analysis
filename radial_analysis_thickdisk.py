e6ce94d6405e674d92b5e24e6acc7d721d52774f
LAPTOP-6K8H7DVQ\Russell <rlgraf@ucdavis.edu>
2022-12-12 14:26:43 -0800

    edit

--- radial_analysis_thickdisk.py
+++ radial_analysis_thickdisk.py
@@ -8,7 +8,7 @@
 #SBATCH --ntasks=1  # (MPI) tasks total
 #SBATCH --cpus-per-task=1  # (OpenMP) threads per (MPI) task
 #SBATCH --time=03:00:00
-#SBATCH --output=radial_analysis_thickdisk_%j.txt
+#SBATCH --output=radial_analysis_thickdisk%j.txt
 #SBATCH --mail-user=rlgraf@ucdavis.edu
 #SBATCH --mail-type=fail
 #SBATCH --mail-type=begin
@@ -51,12 +51,17 @@ def R90_func():
     R90_Thelma = np.array([15.1, 13, 9.9, 8.2, 7.7, 7.8, 7.6, 9, 8.8, 9.6, 6.5, 5.6, 3.1, 0.6])
     R90_Louise = np.array([17.3, 16.4, 14.5, 12.7, 12.4, 11.2, 9.2, 5.1, 5.1, 7.6, 5.4, 4.7, 5.6, 1.1])
     
+    
     R90 = np.vstack([R90_m12i, R90_m12c, R90_m12f,  R90_m12m, R90_m12b, R90_Romeo, R90_Juliet, R90_Romulus, R90_Remus, R90_Thelma, R90_Louise])
     return(R90)
 
+def R90_z_0_func():
+    R90_z_0 = np.array([12.7,11.8,17.0,12.7,11.6,16.8,16,16.9,16.2,15.1,17.3])
+    return(R90_z_0)
+
 # z = 0
 
-def Fe_H_agedependent(x1,x2,x3,x4,x5,x6,x7,x8,a1,a2,r,r_form,age,part):
+def Fe_H_agedependent(x1,x2,x3,x4,x5,x6,x7,x8,a1,a2,r,r_form,age,part, particle_thresh = 100):
     index = ut.array.get_indices(r[:,0], [x1,x2])
     index2 = ut.array.get_indices(abs(r[:,2]), [x3,x4], prior_indices = index)
     index3 = ut.array.get_indices(r_form[:,0], [x5,x6], prior_indices = index2)
@@ -64,7 +69,7 @@ def Fe_H_agedependent(x1,x2,x3,x4,x5,x6,x7,x8,a1,a2,r,r_form,age,part):
     index5 = ut.array.get_indices(age, [a1,a2], prior_indices = index4)
     Fe_H = part['star'].prop('metallicity.iron')
     Fe_H_cut = Fe_H[index5]
-    if len(Fe_H_cut) == 0:
+    if len(Fe_H_cut) < particle_thresh:
         return(np.nan)
     weight_avg = sum((Fe_H_cut)*part['star']['mass'][index5])/sum(part['star']['mass'][index5])
     return(weight_avg)
@@ -76,6 +81,7 @@ def radial_analysis_z_0():
     slope_total = []
     sim = sim_func()
     R90 = R90_func()
+    R90_z_0 = R90_z_0_func()
     LG_counter = 0
     for q, s in enumerate(sim):
         simulation_directory = s
@@ -97,10 +103,10 @@ def radial_analysis_z_0():
             r90 = R90[q+LG_counter]
             for a, b in zip(np.arange(0,14), r90):
                 x = []
-                for i in np.arange(0,b,b/10):
-                    x.append(Fe_H_agedependent(i,i+b/10,2,3,0,b,2,3,a,a+1,r,r_form,age,part))
+                for i in np.arange(0,R90_z_0[q+LG_counter],R90_z_0[q+LG_counter]/10):
+                    x.append(Fe_H_agedependent(i,i+R90_z_0[q+LG_counter]/10,2,3,0,b,2,3,a,a+1,r,r_form,age,part))
                 Fe_H_rad.append(x)
-                l = np.arange(0,b,b/10)
+                l = np.arange(0,R90_z_0[q+LG_counter],R90_z_0[q+LG_counter]/10)
                 x = np.array(x)
                 if np.isnan(x).all():
                     slope.append(np.nan)
@@ -117,7 +123,7 @@ def radial_analysis_z_0():
 
 # formation
 
-def Fe_H_agedependent_form(x1,x2,x3,x4,x5,x6,x7,x8,a1,a2,r_form,r,age,part):
+def Fe_H_agedependent_form(x1,x2,x3,x4,x5,x6,x7,x8,a1,a2,r_form,r,age,part, particle_thresh = 100):
     index = ut.array.get_indices(r_form[:,0], [x1,x2])
     index2 = ut.array.get_indices(abs(r_form[:,2]), [x3,x4], prior_indices = index)
     index3 = ut.array.get_indices(r[:,0],[x5,x6], prior_indices = index2)
@@ -125,7 +131,7 @@ def Fe_H_agedependent_form(x1,x2,x3,x4,x5,x6,x7,x8,a1,a2,r_form,r,age,part):
     index5 = ut.array.get_indices(age, [a1,a2], prior_indices = index4)
     Fe_H = part['star'].prop('metallicity.iron')
     Fe_H_cut = Fe_H[index5]
-    if len(Fe_H_cut) == 0:
+    if len(Fe_H_cut) < particle_thresh:
         return(np.nan)
     weight_avg = sum((Fe_H_cut)*part['star']['mass'][index5])/sum(part['star']['mass'][index5])
     return(weight_avg)
@@ -159,14 +165,14 @@ def radial_analysis_form():
             for a_f, b_f in zip(np.arange(0,14), r90):
                 x_f = []
                 for i_f in np.arange(0,b_f,b_f/10):
-                    x_f.append(Fe_H_agedependent_form(i_f,i_f+b_f/10,2,3,0,b_f,2,3,a_f,a_f+1,r_form,r,age,part))
+                    x_f.append(Fe_H_agedependent_form(i_f,i_f+b_f/10,2,3,0,20,2,3,a_f,a_f+1,r_form,r,age,part))
                 Fe_H_rad_form.append(x_f)
                 l_f = np.arange(0,b_f,b_f/10)
                 x_f = np.array(x_f)
                 if np.isnan(x_f).all():
                     slope_form.append(np.nan)
                 else:
-                    j_f, k_f = np.polyfit(l_f[np.isfinite(x_f)], x_f[np.isfinite(x_f)], 1)
+                    j_f, k_f = np.polyfit(l_f[np.isfinite(x_f)],x_f[np.isfinite(x_f)],1)
                     slope_form.append(j_f)
             Fe_H_rad_form_total.append(Fe_H_rad_form)
             slope_form_total.append(slope_form)
