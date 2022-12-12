e44af45466823b8c17a5be55024fb111b5c2b43c
LAPTOP-6K8H7DVQ\Russell <rlgraf@ucdavis.edu>
2022-12-11 16:08:11 -0800

    edit

--- azimuthal_angmom_analysis.py
+++ azimuthal_angmom_analysis.py
@@ -91,10 +91,10 @@ def azimuthal_analysis_z_0():
             Fe_H_azim = []
             angmom_totvalues = part['star'].prop('host.velocity.principal.cylindrical')[:,1]*r[:,0]
             angmom_max = angmom_func(7,8,0,3,7,8,0,3,0,1,r,r_form,age,part,particle_thresh = 100)
-            for a in np.arange(0,1,0.05):
+            for i in np.arange(0,angmom_max,angmom_max/15):
                 std_vs_rad = []
-                for i in np.arange(0,angmom_max,angmom_max/15):
-                    std_vs_rad.append(Fe_H_agedependent_sd(i,i+angmom_max/15,0,3,0,angmom_max,0,3,0,1,0,360,r,r_form,age,part,angmom_totvalues,particle_thresh = 100))
+                for a in np.arange(0,1,0.05):
+                    std_vs_rad.append(Fe_H_agedependent_sd(i,i+angmom_max/15,0,3,0,angmom_max,0,3,a,a+0.05,0,360,r,r_form,age,part,angmom_totvalues,particle_thresh = 100))
                 std_vs_rad = np.array(std_vs_rad)
                 std_vs_rad_mean = np.nanmean(std_vs_rad,0)
                 Fe_H_azim.append(std_vs_rad_mean)                                     
@@ -156,10 +156,10 @@ def azimuthal_analysis_form():
             Fe_H_azim_form = []
             angmom_totvalues_form = part['star'].prop('host.velocity.principal.cylindrical')[:,1]*r_form[:,0]
             angmom_max_form = angmom_form_func(7,8,0,3,7,8,0,3,0,1,r,r_form,age,part,particle_thresh = 100)
-            for a_f in np.arange(0,1,0.05):
+            for i_f in np.arange(0,angmom_max_form,angmom_max_form/15
                 std_vs_rad_f = []
-                for i_f in np.arange(0,angmom_max_form,angmom_max_form/15):
-                    std_vs_rad_f.append(Fe_H_agedependent_sd_form(i_f,i_f+angmom_max_form/15,0,3,0,angmom_max_form,0,3,0,1,0,360,r,r_form,age,part,angmom_totvalues_form,particle_thresh = 100))
+                for a_f in np.arange(0,1,0.05):
+                    std_vs_rad_f.append(Fe_H_agedependent_sd_form(i_f,i_f+angmom_max_form/15,0,3,0,angmom_max_form,0,3,a,a+0.05,0,360,r,r_form,age,part,angmom_totvalues_form,particle_thresh = 100))
                 std_vs_rad_f = np.array(std_vs_rad_f)
                 std_vs_rad_f_mean = np.nanmean(std_vs_rad_f,0)
                 Fe_H_azim_form.append(std_vs_rad_f_mean)                                     
