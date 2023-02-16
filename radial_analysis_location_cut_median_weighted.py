b591bad1e2507ff0998a1983f28fd9850dedd69b
LAPTOP-6K8H7DVQ\Russell <rlgraf@ucdavis.edu>
2023-02-16 11:49:40 -0800

    edit

--- radial_analysis_location_cut_median_weighted.py
+++ radial_analysis_location_cut_median_weighted.py
@@ -101,10 +101,10 @@ def radial_analysis_z_0():
             r90 = R90
             for a, b in zip(np.arange(0,14), r90):
                 x = []
-                for i in np.arange(0,15,1):
-                    x.append(Fe_H_agedependent(i,i+1,-3,3,0,b,-3,3,a,a+1,r,r_form,age,part))
+                for i in np.arange(0,15,15/10):
+                    x.append(Fe_H_agedependent(i,i+15/10,-3,3,0,b,-3,3,a,a+1,r,r_form,age,part))
                 Fe_H_rad.append(x)
-                l = np.arange(0,15,1)
+                l = np.arange(0,15,15/10)
                 x = np.array(x)
                 if np.isnan(x).all():
                     slope.append(np.nan)
