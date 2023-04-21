e656dcc96394ddc605cea048f51861692a1b0fed
LAPTOP-6K8H7DVQ\Russell <rlgraf@ucdavis.edu>
2023-04-21 14:30:21 -0700

    edit

--- vertical_analysis_location_cut_median.py
+++ vertical_analysis_location_cut_median.py
@@ -179,5 +179,4 @@ def vertical_analysis_form():
     ut_io.file_hdf5('/home/rlgraf/Final_Figures/VER_slope_r_4_5_form_location_cut_median_weighted', slope_ver_r_4_5_form_total)
     ut_io.file_hdf5('/home/rlgraf/Final_Figures/VER_slope_r_7_8_form_location_cut_median_weighted', slope_ver_r_7_8_form_total)
     
-vertical_analysis_z_0()
 vertical_analysis_form()
