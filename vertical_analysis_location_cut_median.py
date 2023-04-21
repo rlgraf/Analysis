9ecb38fd4236b3cd97f12011d77438edf2763f31
LAPTOP-6K8H7DVQ\Russell <rlgraf@ucdavis.edu>
2023-04-21 14:28:03 -0700

    edit

--- vertical_analysis_location_cut_median.py
+++ vertical_analysis_location_cut_median.py
@@ -34,152 +34,6 @@ def sim_func():
     return(sim)
 
 
-# z = 0
-
-def Fe_H_agedependent(x1,x2,x3,x4,x5,x6,x7,x8,a1,a2,r,r_form,age,part, particle_thresh = 1):
-    index = ut.array.get_indices(r[:,0], [x1,x2])
-    index2 = ut.array.get_indices(abs(r[:,2]), [x3,x4], prior_indices = index)
-    index3 = ut.array.get_indices(r_form[:,0], [x5,x6], prior_indices = index2)
-    index4 = ut.array.get_indices(abs(r_form[:,2]), [x7,x8], prior_indices = index3)
-    index5 = ut.array.get_indices(age, [a1,a2], prior_indices = index4)
-    Fe_H = part['star'].prop('metallicity.iron')
-    Fe_H_cut = Fe_H[index5]
-    if len(Fe_H_cut) < particle_thresh:
-        return(np.nan)
-    weight_avg = ws.numpy_weighted_median(Fe_H_cut, part['star']['mass'][index5])
-    return(weight_avg)
-
-def vertical_analysis_z_0():
-    
-    Fe_H_ver_r_1_2_total = []
-    slope_ver_r_1_2_total = []
-    sim = sim_func()
-    for s in sim:
-        simulation_directory = s
-        part = gizmo.io.Read.read_snapshots(['star'], 'redshift', 0, simulation_directory, assign_hosts_rotation=True, assign_formation_coordinates = True)
-        Fe_H = part['star'].prop('metallicity.iron')
-        age = part['star'].prop('age')
-        
-        if s in ['/share/wetzellab/m12_elvis/m12_elvis_RomeoJuliet_r3500', '/share/wetzellab/m12_elvis/m12_elvis_RomulusRemus_r4000', '/share/wetzellab/m12_elvis/m12_elvis_ThelmaLouise_r4000']:
-            r_array = [part['star'].prop('host1.distance.principal.cylindrical'), part['star'].prop('host2.distance.principal.cylindrical')]
-            r_form_array = [part['star'].prop('form.host1.distance.principal.cylindrical'), part['star'].prop('form.host2.distance.principal.cylindrical')]
-        else:           
-            r_array = [part['star'].prop('host.distance.principal.cylindrical')]
-            r_form_array = [part['star'].prop('form.host.distance.principal.cylindrical')]    
-            
-        for r, r_form in zip(r_array, r_form_array):    
-            Fe_H_ver_r_1_2 = []
-            slope_ver_r_1_2 = []
-            for a in np.arange(0,14):
-                Fe_H_ver_pre = []
-                for a_pre in np.arange(0,1,0.1):
-                    x = []
-                    for i in np.arange(0,1,0.1):
-                        x.append(Fe_H_agedependent(1,2,i,i+0.1,0,15,0,1,a+a_pre,a+a_pre+0.1,r,r_form,age,part))
-                    Fe_H_ver_pre.append(x)
-                Fe_H_ver_pre = np.array(Fe_H_ver_pre)
-                Fe_H_ver_pre_mean = np.nanmean(Fe_H_ver_pre,0)
-                Fe_H_ver_r_1_2.append(Fe_H_ver_pre_mean)
-                l = np.arange(0,1,0.1)
-                #x = np.array(x)
-                if len(Fe_H_ver_pre_mean) == 0:
-                    slope_ver_r_1_2.append(np.nan)
-                else:
-                    j, k = np.polyfit(l[np.isfinite(Fe_H_ver_pre_mean)],Fe_H_ver_pre_mean[np.isfinite(Fe_H_ver_pre_mean)],1)
-                    slope_ver_r_1_2.append(j)
-            Fe_H_ver_r_1_2_total.append(Fe_H_ver_r_1_2)
-            slope_ver_r_1_2_total.append(slope_ver_r_1_2)
-    Fe_H_ver_r_1_2_total = np.array([Fe_H_ver_r_1_2_total])
-    slope_ver_r_1_2_total = np.array([slope_ver_r_1_2_total])
-
-    Fe_H_ver_r_4_5_total = []
-    slope_ver_r_4_5_total = []
-    sim = sim_func()
-    for s in sim:
-        simulation_directory = s
-        part = gizmo.io.Read.read_snapshots(['star'], 'redshift', 0, simulation_directory, assign_hosts_rotation=True, assign_formation_coordinates = True)
-        Fe_H = part['star'].prop('metallicity.iron')
-        age = part['star'].prop('age')
-                             
-        if s in ['/share/wetzellab/m12_elvis/m12_elvis_RomeoJuliet_r3500', '/share/wetzellab/m12_elvis/m12_elvis_RomulusRemus_r4000', '/share/wetzellab/m12_elvis/m12_elvis_ThelmaLouise_r4000']:
-            r_array = [part['star'].prop('host1.distance.principal.cylindrical'), part['star'].prop('host2.distance.principal.cylindrical')]
-            r_form_array = [part['star'].prop('form.host1.distance.principal.cylindrical'), part['star'].prop('form.host2.distance.principal.cylindrical')]
-        else:           
-            r_array = [part['star'].prop('host.distance.principal.cylindrical')]
-            r_form_array = [part['star'].prop('form.host.distance.principal.cylindrical')]
-            
-        for r, r_form in zip(r_array, r_form_array):
-            Fe_H_ver_r_4_5 = []
-            slope_ver_r_4_5 = []
-            for a in np.arange(0,14):
-                Fe_H_ver_pre = []
-                for a_pre in np.arange(0,1,0.1):
-                    x = []
-                    for i in np.arange(0,1,0.1):
-                        x.append(Fe_H_agedependent(4,5,i,i+0.1,0,15,0,1,a+a_pre,a+a_pre+0.1,r,r_form,age,part))
-                    Fe_H_ver_pre.append(x)
-                Fe_H_ver_pre = np.array(Fe_H_ver_pre)
-                Fe_H_ver_pre_mean = np.nanmean(Fe_H_ver_pre,0)
-                Fe_H_ver_r_4_5.append(Fe_H_ver_pre_mean)
-                l = np.arange(0,1,0.1)
-                if len(Fe_H_ver_pre_mean) == 0:
-                    slope_ver_r_4_5.append(np.nan)
-                else:
-                    j, k = np.polyfit(l[np.isfinite(Fe_H_ver_pre_mean)],Fe_H_ver_pre_mean[np.isfinite(Fe_H_ver_pre_mean)],1)
-                    slope_ver_r_4_5.append(j)
-            Fe_H_ver_r_4_5_total.append(Fe_H_ver_r_4_5)
-            slope_ver_r_4_5_total.append(slope_ver_r_4_5)
-    Fe_H_ver_r_4_5_total = np.array([Fe_H_ver_r_4_5_total])
-    slope_ver_r_4_5_total = np.array([slope_ver_r_4_5_total])
-
-    Fe_H_ver_r_7_8_total = []
-    slope_ver_r_7_8_total = []
-    sim = sim_func()
-    for s in sim:
-        simulation_directory = s
-        part = gizmo.io.Read.read_snapshots(['star'], 'redshift', 0, simulation_directory, assign_hosts_rotation=True, assign_formation_coordinates = True)
-        Fe_H = part['star'].prop('metallicity.iron')
-        age = part['star'].prop('age')
-        
-        if s in ['/share/wetzellab/m12_elvis/m12_elvis_RomeoJuliet_r3500', '/share/wetzellab/m12_elvis/m12_elvis_RomulusRemus_r4000', '/share/wetzellab/m12_elvis/m12_elvis_ThelmaLouise_r4000']:
-            r_array = [part['star'].prop('host1.distance.principal.cylindrical'), part['star'].prop('host2.distance.principal.cylindrical')]
-            r_form_array = [part['star'].prop('form.host1.distance.principal.cylindrical'), part['star'].prop('form.host2.distance.principal.cylindrical')]
-        else:           
-            r_array = [part['star'].prop('host.distance.principal.cylindrical')]
-            r_form_array = [part['star'].prop('form.host.distance.principal.cylindrical')]    
-            
-        for r, r_form in zip(r_array, r_form_array):
-            Fe_H_ver_r_7_8 = []
-            slope_ver_r_7_8 = []
-            for a in np.arange(0,14):
-                Fe_H_ver_pre = []
-                for a_pre in np.arange(0,1,0.1):
-                    x = []
-                    for i in np.arange(0,1,0.1):
-                        x.append(Fe_H_agedependent(7,8,i,i+0.1,0,15,0,1,a+a_pre,a+a_pre+0.1,r,r_form,age,part))
-                    Fe_H_ver_pre.append(x)
-                Fe_H_ver_pre = np.array(Fe_H_ver_pre)
-                Fe_H_ver_pre_mean = np.nanmean(Fe_H_ver_pre,0)
-                Fe_H_ver_r_7_8.append(Fe_H_ver_pre_mean)
-                l = np.arange(0,1,0.1)
-                if len(Fe_H_ver_pre_mean) == 0:
-                    slope_ver_r_7_8.append(np.nan)
-                else:
-                    j, k = np.polyfit(l[np.isfinite(Fe_H_ver_pre_mean)],Fe_H_ver_pre_mean[np.isfinite(Fe_H_ver_pre_mean)],1)
-                    slope_ver_r_7_8.append(j)
-            Fe_H_ver_r_7_8_total.append(Fe_H_ver_r_7_8)
-            slope_ver_r_7_8_total.append(slope_ver_r_7_8)
-    Fe_H_ver_r_7_8_total = np.array([Fe_H_ver_r_7_8_total])
-    slope_ver_r_7_8_total = np.array([slope_ver_r_7_8_total])
-
-    ut_io.file_hdf5('/home/rlgraf/Final_Figures/VER_profile_r_1_2_z_0_location_cut_median_weighted', Fe_H_ver_r_1_2_total)
-    ut_io.file_hdf5('/home/rlgraf/Final_Figures/VER_profile_r_4_5_z_0_location_cut_median_weighted', Fe_H_ver_r_4_5_total)
-    ut_io.file_hdf5('/home/rlgraf/Final_Figures/VER_profile_r_7_8_z_0_location_cut_median_weighted', Fe_H_ver_r_7_8_total)
-    
-    ut_io.file_hdf5('/home/rlgraf/Final_Figures/VER_slope_r_1_2_z_0_location_cut_median_weighted', slope_ver_r_1_2_total)
-    ut_io.file_hdf5('/home/rlgraf/Final_Figures/VER_slope_r_4_5_z_0_location_cut_median_weighted', slope_ver_r_4_5_total)
-    ut_io.file_hdf5('/home/rlgraf/Final_Figures/VER_slope_r_7_8_z_0_location_cut_median_weighted', slope_ver_r_7_8_total)
-
 # formation
 
 def Fe_H_agedependent_form(x1,x2,x3,x4,x5,x6,x7,x8,a1,a2,r_form,r,age,part, particle_thresh = 1):
@@ -227,7 +81,7 @@ def vertical_analysis_form():
                 Fe_H_ver_pre_f_mean = np.nanmean(Fe_H_ver_pre_f,0)
                 Fe_H_ver_r_1_2_form.append(Fe_H_ver_pre_f_mean)
                 l_f = np.arange(0,1,0.1)
-                if len(Fe_H_ver_pre_f_mean) == 0:
+                if np.isnan(Fe_H_ver_pre_f_mean).all():
                     slope_ver_r_1_2_form.append(np.nan)
                 else:
                     j_f, k_f = np.polyfit(l_f[np.isfinite(Fe_H_ver_pre_f_mean)],Fe_H_ver_pre_f_mean[np.isfinite(Fe_H_ver_pre_f_mean)],1)
@@ -267,7 +121,7 @@ def vertical_analysis_form():
                 Fe_H_ver_pre_f_mean = np.nanmean(Fe_H_ver_pre_f,0)
                 Fe_H_ver_r_4_5_form.append(Fe_H_ver_pre_f_mean)
                 l_f = np.arange(0,1,0.1)
-                if len(Fe_H_ver_pre_f_mean) == 0:
+                if np.isnan(Fe_H_ver_pre_f_mean).all():
                     slope_ver_r_4_5_form.append(np.nan)
                 else:
                     j_f, k_f = np.polyfit(l_f[np.isfinite(Fe_H_ver_pre_f_mean)],Fe_H_ver_pre_f_mean[np.isfinite(Fe_H_ver_pre_f_mean)],1)
@@ -307,7 +161,7 @@ def vertical_analysis_form():
                 Fe_H_ver_pre_f_mean = np.nanmean(Fe_H_ver_pre_f,0)
                 Fe_H_ver_r_7_8_form.append(Fe_H_ver_pre_f_mean)
                 l_f = np.arange(0,1,0.1)
-                if len(Fe_H_ver_pre_f_mean) == 0:
+                if np.isnan(Fe_H_ver_pre_f_mean).all():
                     slope_ver_r_7_8_form.append(np.nan)
                 else:
                     j_f, k_f = np.polyfit(l_f[np.isfinite(Fe_H_ver_pre_f_mean)],Fe_H_ver_pre_f_mean[np.isfinite(Fe_H_ver_pre_f_mean)],1)
