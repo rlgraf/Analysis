d38aa89fb81ec7be1436794edb3e85a94aa22e7d
LAPTOP-6K8H7DVQ\Russell <rlgraf@ucdavis.edu>
2023-09-21 20:41:48 -0700

    edit

--- vertical_analysis_location_cut_median.py
+++ vertical_analysis_location_cut_median.py
@@ -30,9 +30,145 @@ import weightedstats as ws
 
 
 def sim_func():
-    sim = ['/share/wetzellab/m12i/m12i_r7100_uvb-late/', '/share/wetzellab/m12c/m12c_r7100', '/share/wetzellab/m12f/m12f_r7100',  '/share/wetzellab/m12m/m12m_r7100','/share/wetzellab/m12b/m12b_r7100','/share/wetzellab/m12_elvis/m12_elvis_RomeoJuliet_r3500', '/share/wetzellab/m12_elvis/m12_elvis_RomulusRemus_r4000', '/share/wetzellab/m12_elvis/m12_elvis_ThelmaLouise_r4000']
+    sim = ['/group/awetzelgrp/m12i/m12i_r7100_uvb-late/', '/group/awetzelgrp/m12c/m12c_r7100', '/group/awetzelgrp/m12f/m12f_r7100',  '/group/awetzelgrp/m12m/m12m_r7100','/group/awetzelgrp/m12b/m12b_r7100', '/group/awetzelgrp/m12_elvis/m12_elvis_RomeoJuliet_r3500', '/group/awetzelgrp/m12_elvis/m12_elvis_RomulusRemus_r4000', '/group/awetzelgrp/m12_elvis/m12_elvis_ThelmaLouise_r4000']
     return(sim)
 
+# z = 0
+
+def Fe_H_agedependent(x1,x2,x3,x4,x5,x6,x7,x8,a1,a2,r,r_form,age,part, particle_thresh = 1):
+    index = ut.array.get_indices(r[:,0], [x1,x2])
+    index2 = ut.array.get_indices(abs(r[:,2]), [x3,x4], prior_indices = index)
+    index3 = ut.array.get_indices(r_form[:,0],[x5,x6], prior_indices = index2)
+    index4 = ut.array.get_indices(abs(r_form[:,2]), [x7,x8], prior_indices = index3)
+    index5 = ut.array.get_indices(age, [a1,a2], prior_indices = index4)
+    Fe_H = part['star'].prop('metallicity.iron')
+    Fe_H_cut = Fe_H[index5]
+    if len(Fe_H_cut) < particle_thresh:
+        return(np.nan)
+    weight_avg = ws.numpy_weighted_median(Fe_H_cut, part['star']['mass'][index5])
+    return(weight_avg)
+
+def vertical_analysis_z_0():
+    
+    Fe_H_ver_r_1_2_z_0_total = []
+    slope_ver_r_1_2_z_0_total = []
+    sim = sim_func()
+    for s in sim:
+        simulation_directory = s
+        part = gizmo.io.Read.read_snapshots(['star'], 'redshift', 0, simulation_directory, assign_hosts_rotation=True, assign_formation_coordinates = True)
+        Fe_H = part['star'].prop('metallicity.iron')
+        age = part['star'].prop('age')
+        
+        if s in ['/group/awetzelgrp/m12_elvis/m12_elvis_RomeoJuliet_r3500', '/group/awetzelgrp/m12_elvis/m12_elvis_RomulusRemus_r4000', '/group/awetzelgrp/m12_elvis/m12_elvis_ThelmaLouise_r4000']:
+            r_array = [part['star'].prop('host1.distance.principal.cylindrical'), part['star'].prop('host2.distance.principal.cylindrical')]
+            r_form_array = [part['star'].prop('form.host1.distance.principal.cylindrical'), part['star'].prop('form.host2.distance.principal.cylindrical')]
+        else:
+            r_array = [part['star'].prop('host.distance.principal.cylindrical')]
+            r_form_array = [part['star'].prop('form.host.distance.principal.cylindrical')]  
+        
+        for r, r_form in zip(r_array, r_form_array):
+            Fe_H_ver_r_1_2_z_0 = []
+            slope_ver_r_1_2_z_0 = []
+            for a in np.arange(0,14):
+                Fe_H_ver_pre = []
+                for a_pre in np.arange(0,1,0.1):
+                    x = []
+                    for i in np.arange(0,1,0.1):
+                        x.append(Fe_H_agedependent(3.5,4.5,i,i+0.1,0,15,0,1,a+a_pre,a+a_pre+0.1,r,r_form,age,part))
+                    Fe_H_ver_pre.append(x)
+                Fe_H_ver_pre = np.array(Fe_H_ver_pre)
+                Fe_H_ver_pre_mean = np.nanmean(Fe_H_ver_pre,0)
+                Fe_H_ver_r_1_2_z_0.append(Fe_H_ver_pre_mean)
+                l = np.arange(0,1,0.1)
+                if sum(np.isfinite(Fe_H_ver_pre_mean)) < 2:
+                    slope_ver_r_1_2_z_0.append(np.nan)
+                else:
+                    j, k = np.polyfit(l[np.isfinite(Fe_H_ver_pre_mean)],Fe_H_ver_pre_mean[np.isfinite(Fe_H_ver_pre_mean)],1)
+                    slope_ver_r_1_2_z_0.append(j)
+            Fe_H_ver_r_1_2_z_0_total.append(Fe_H_ver_r_1_2_z_0)
+            slope_ver_r_1_2_z_0_total.append(slope_ver_r_1_2_z_0)
+    Fe_H_ver_r_1_2_z_0_total = np.array([Fe_H_ver_r_1_2_z_0_total])
+    slope_ver_r_1_2_z_0_total = np.array([slope_ver_r_1_2_z_0_total])
+
+    Fe_H_ver_r_4_5_z_0_total = []
+    slope_ver_r_4_5_z_0_total = []
+    sim = sim_func()
+    for s in sim:
+        simulation_directory = s
+        part = gizmo.io.Read.read_snapshots(['star'], 'redshift', 0, simulation_directory, assign_hosts_rotation=True, assign_formation_coordinates = True)
+        Fe_H = part['star'].prop('metallicity.iron')
+        age = part['star'].prop('age')
+    
+        if s in ['/group/awetzelgrp/m12_elvis/m12_elvis_RomeoJuliet_r3500', '/group/awetzelgrp/m12_elvis/m12_elvis_RomulusRemus_r4000', '/group/awetzelgrp/m12_elvis/m12_elvis_ThelmaLouise_r4000']:
+            r_array = [part['star'].prop('host1.distance.principal.cylindrical'), part['star'].prop('host2.distance.principal.cylindrical')]
+            r_form_array = [part['star'].prop('form.host1.distance.principal.cylindrical'), part['star'].prop('form.host2.distance.principal.cylindrical')]
+        else:
+            r_array = [part['star'].prop('host.distance.principal.cylindrical')]
+            r_form_array = [part['star'].prop('form.host.distance.principal.cylindrical')]
+        
+        for r, r_form in zip(r_array, r_form_array):
+            Fe_H_ver_r_4_5_z_0 = []
+            slope_ver_r_4_5_z_0 = []
+            for a in np.arange(0,14):
+                Fe_H_ver_pre = []
+                for a_pre in np.arange(0,1,0.1):
+                    x = []
+                    for i in np.arange(0,1,0.1):
+                        x.append(Fe_H_agedependent(7.5,8.5,i,i+0.1,0,15,0,1,a+a_pre,a+a_pre+0.1,r,r_form,age,part))
+                    Fe_H_ver_pre.append(x)
+                Fe_H_ver_pre = np.array(Fe_H_ver_pre)
+                Fe_H_ver_pre_mean = np.nanmean(Fe_H_ver_pre,0)
+                Fe_H_ver_r_4_5_z_0.append(Fe_H_ver_pre_mean)
+                l = np.arange(0,1,0.1)
+                if sum(np.isfinite(Fe_H_ver_pre_mean)) < 2:
+                    slope_ver_r_4_5_z_0.append(np.nan)
+                else:
+                    j, k = np.polyfit(l[np.isfinite(Fe_H_ver_pre_mean)],Fe_H_ver_pre_mean[np.isfinite(Fe_H_ver_pre_mean)],1)
+                    slope_ver_r_4_5_z_0.append(j)
+            Fe_H_ver_r_4_5_z_0_total.append(Fe_H_ver_r_4_5_z_0)
+            slope_ver_r_4_5_z_0_total.append(slope_ver_r_4_5_z_0)
+    Fe_H_ver_r_4_5_z_0_total = np.array([Fe_H_ver_r_4_5_z_0_total])
+    slope_ver_r_4_5_z_0_total = np.array([slope_ver_r_4_5_z_0_total])
+
+    Fe_H_ver_r_7_8_z_0_total = []
+    slope_ver_r_7_8_z_0_total = []
+    sim = sim_func()
+    for s in sim:
+        simulation_directory = s
+        part = gizmo.io.Read.read_snapshots(['star'], 'redshift', 0, simulation_directory, assign_hosts_rotation=True, assign_formation_coordinates = True)
+        Fe_H = part['star'].prop('metallicity.iron')
+        age = part['star'].prop('age')
+    
+        if s in ['/group/awetzelgrp/m12_elvis/m12_elvis_RomeoJuliet_r3500', '/group/awetzelgrp/m12_elvis/m12_elvis_RomulusRemus_r4000', '/group/awetzelgrp/m12_elvis/m12_elvis_ThelmaLouise_r4000']:
+            r_array = [part['star'].prop('host1.distance.principal.cylindrical'), part['star'].prop('host2.distance.principal.cylindrical')]
+            r_form_array = [part['star'].prop('form.host1.distance.principal.cylindrical'), part['star'].prop('form.host2.distance.principal.cylindrical')]
+        else:
+            r_array = [part['star'].prop('host.distance.principal.cylindrical')]
+            r_form_array = [part['star'].prop('form.host.distance.principal.cylindrical')]
+            
+        for r, r_form in zip(r_array, r_form_array):
+            Fe_H_ver_r_7_8_z_0 = []
+            slope_ver_r_7_8_z_0 = []
+            for a in np.arange(0,14):
+                Fe_H_ver_pre = []
+                for a_pre in np.arange(0,1,0.1):
+                    x = []
+                    for i in np.arange(0,1,0.1):
+                        x.append(Fe_H_agedependent(11.5,12.5,i,i+0.1,0,15,0,1,a+a_pre,a+a_pre+0.1,r,r_form,age,part))
+                    Fe_H_ver_pre.append(x)
+                Fe_H_ver_pre = np.array(Fe_H_ver_pre)
+                Fe_H_ver_pre_mean = np.nanmean(Fe_H_ver_pre,0)
+                Fe_H_ver_r_7_8_z_0.append(Fe_H_ver_pre_mean)
+                l = np.arange(0,1,0.1)
+                if sum(np.isfinite(Fe_H_ver_pre_mean)) < 2:
+                    slope_ver_r_7_8_z_0.append(np.nan)
+                else:
+                    j, k = np.polyfit(l[np.isfinite(Fe_H_ver_pre_mean)],Fe_H_ver_pre_mean[np.isfinite(Fe_H_ver_pre_mean)],1)
+                    slope_ver_r_7_8_z_0.append(j)
+            Fe_H_ver_r_7_8_z_0_total.append(Fe_H_ver_r_7_8_z_0)
+            slope_ver_r_7_8_z_0_total.append(slope_ver_r_7_8_z_0)
+    Fe_H_ver_r_7_8_z_0_total = np.array([Fe_H_ver_r_7_8_z_0_total])
+    slope_ver_r_7_8_z_0_total = np.array([slope_ver_r_7_8_z_0_total])
 
 # formation
 
@@ -60,12 +196,12 @@ def vertical_analysis_form():
         Fe_H = part['star'].prop('metallicity.iron')
         age = part['star'].prop('age')
         
-        if s in ['/share/wetzellab/m12_elvis/m12_elvis_RomeoJuliet_r3500', '/share/wetzellab/m12_elvis/m12_elvis_RomulusRemus_r4000', '/share/wetzellab/m12_elvis/m12_elvis_ThelmaLouise_r4000']:
+        if s in ['/group/awetzelgrp/m12_elvis/m12_elvis_RomeoJuliet_r3500', '/group/awetzelgrp/m12_elvis/m12_elvis_RomulusRemus_r4000', '/group/awetzelgrp/m12_elvis/m12_elvis_ThelmaLouise_r4000']:
             r_array = [part['star'].prop('host1.distance.principal.cylindrical'), part['star'].prop('host2.distance.principal.cylindrical')]
             r_form_array = [part['star'].prop('form.host1.distance.principal.cylindrical'), part['star'].prop('form.host2.distance.principal.cylindrical')]
-        else:           
+        else:
             r_array = [part['star'].prop('host.distance.principal.cylindrical')]
-            r_form_array = [part['star'].prop('form.host.distance.principal.cylindrical')]   
+            r_form_array = [part['star'].prop('form.host.distance.principal.cylindrical')]
         
         for r, r_form in zip(r_array, r_form_array):
             Fe_H_ver_r_1_2_form = []
@@ -75,7 +211,7 @@ def vertical_analysis_form():
                 for a_pre_f in np.arange(0,1,0.1):
                     x_f = []
                     for i_f in np.arange(0,1,0.1):
-                        x_f.append(Fe_H_agedependent_form(1,2,i_f,i_f+0.1,0,15,0,1,a_f+a_pre_f,a_f+a_pre_f+0.1,r_form,r,age,part))
+                        x_f.append(Fe_H_agedependent_form(3.5,4.5,i_f,i_f+0.1,0,15,0,1,a_f+a_pre_f,a_f+a_pre_f+0.1,r_form,r,age,part))
                     Fe_H_ver_pre_f.append(x_f)
                 Fe_H_ver_pre_f = np.array(Fe_H_ver_pre_f)
                 Fe_H_ver_pre_f_mean = np.nanmean(Fe_H_ver_pre_f,0)
@@ -100,12 +236,12 @@ def vertical_analysis_form():
         Fe_H = part['star'].prop('metallicity.iron')
         age = part['star'].prop('age')
     
-        if s in ['/share/wetzellab/m12_elvis/m12_elvis_RomeoJuliet_r3500', '/share/wetzellab/m12_elvis/m12_elvis_RomulusRemus_r4000', '/share/wetzellab/m12_elvis/m12_elvis_ThelmaLouise_r4000']:
+        if s in ['/group/awetzelgrp/m12_elvis/m12_elvis_RomeoJuliet_r3500', '/group/awetzelgrp/m12_elvis/m12_elvis_RomulusRemus_r4000', '/group/awetzelgrp/m12_elvis/m12_elvis_ThelmaLouise_r4000']:
             r_array = [part['star'].prop('host1.distance.principal.cylindrical'), part['star'].prop('host2.distance.principal.cylindrical')]
             r_form_array = [part['star'].prop('form.host1.distance.principal.cylindrical'), part['star'].prop('form.host2.distance.principal.cylindrical')]
-        else:           
+        else:
             r_array = [part['star'].prop('host.distance.principal.cylindrical')]
-            r_form_array = [part['star'].prop('form.host.distance.principal.cylindrical')]   
+            r_form_array = [part['star'].prop('form.host.distance.principal.cylindrical')]
         
         for r, r_form in zip(r_array, r_form_array):
             Fe_H_ver_r_4_5_form = []
@@ -115,7 +251,7 @@ def vertical_analysis_form():
                 for a_pre_f in np.arange(0,1,0.1):
                     x_f = []
                     for i_f in np.arange(0,1,0.1):
-                        x_f.append(Fe_H_agedependent_form(4,5,i_f,i_f+0.1,0,15,0,1,a_f+a_pre_f,a_f+a_pre_f+0.1,r_form,r,age,part))
+                        x_f.append(Fe_H_agedependent_form(7.5,8.5,i_f,i_f+0.1,0,15,0,1,a_f+a_pre_f,a_f+a_pre_f+0.1,r_form,r,age,part))
                     Fe_H_ver_pre_f.append(x_f)
                 Fe_H_ver_pre_f = np.array(Fe_H_ver_pre_f)
                 Fe_H_ver_pre_f_mean = np.nanmean(Fe_H_ver_pre_f,0)
@@ -140,12 +276,12 @@ def vertical_analysis_form():
         Fe_H = part['star'].prop('metallicity.iron')
         age = part['star'].prop('age')
     
-        if s in ['/share/wetzellab/m12_elvis/m12_elvis_RomeoJuliet_r3500', '/share/wetzellab/m12_elvis/m12_elvis_RomulusRemus_r4000', '/share/wetzellab/m12_elvis/m12_elvis_ThelmaLouise_r4000']:
+        if s in ['/group/awetzelgrp/m12_elvis/m12_elvis_RomeoJuliet_r3500', '/group/awetzelgrp/m12_elvis/m12_elvis_RomulusRemus_r4000', '/group/awetzelgrp/m12_elvis/m12_elvis_ThelmaLouise_r4000']:
             r_array = [part['star'].prop('host1.distance.principal.cylindrical'), part['star'].prop('host2.distance.principal.cylindrical')]
             r_form_array = [part['star'].prop('form.host1.distance.principal.cylindrical'), part['star'].prop('form.host2.distance.principal.cylindrical')]
-        else:           
+        else:
             r_array = [part['star'].prop('host.distance.principal.cylindrical')]
-            r_form_array = [part['star'].prop('form.host.distance.principal.cylindrical')]   
+            r_form_array = [part['star'].prop('form.host.distance.principal.cylindrical')]
             
         for r, r_form in zip(r_array, r_form_array):
             Fe_H_ver_r_7_8_form = []
@@ -155,7 +291,7 @@ def vertical_analysis_form():
                 for a_pre_f in np.arange(0,1,0.1):
                     x_f = []
                     for i_f in np.arange(0,1,0.1):
-                        x_f.append(Fe_H_agedependent_form(7,8,i_f,i_f+0.1,0,15,0,1,a_f+a_pre_f,a_f+a_pre_f+0.1,r_form,r,age,part))
+                        x_f.append(Fe_H_agedependent_form(11.5,12.5,i_f,i_f+0.1,0,15,0,1,a_f+a_pre_f,a_f+a_pre_f+0.1,r_form,r,age,part))
                     Fe_H_ver_pre_f.append(x_f)
                 Fe_H_ver_pre_f = np.array(Fe_H_ver_pre_f)
                 Fe_H_ver_pre_f_mean = np.nanmean(Fe_H_ver_pre_f,0)
@@ -171,12 +307,13 @@ def vertical_analysis_form():
     Fe_H_ver_r_7_8_form_total = np.array([Fe_H_ver_r_7_8_form_total])
     slope_ver_r_7_8_form_total = np.array([slope_ver_r_7_8_form_total])
 
-    ut_io.file_hdf5('/home/rlgraf/Final_Figures/VER_profile_r_1_2_form_location_cut_median_weighted', Fe_H_ver_r_1_2_form_total)
-    ut_io.file_hdf5('/home/rlgraf/Final_Figures/VER_profile_r_4_5_form_location_cut_median_weighted', Fe_H_ver_r_4_5_form_total)
-    ut_io.file_hdf5('/home/rlgraf/Final_Figures/VER_profile_r_7_8_form_location_cut_median_weighted', Fe_H_ver_r_7_8_form_total)
+    ut_io.file_hdf5('/home/rlgraf/Final_Figures/VER_profile_r_4_z_0_location_cut_median_weighted_publish', Fe_H_ver_r_1_2_z_0_total)
+    ut_io.file_hdf5('/home/rlgraf/Final_Figures/VER_profile_r_8_z_0_location_cut_median_weighted_publish', Fe_H_ver_r_4_5_z_0_total)
+    ut_io.file_hdf5('/home/rlgraf/Final_Figures/VER_profile_r_12_z_0_location_cut_median_weighted_publish', Fe_H_ver_r_7_8_z_0_total)
     
-    ut_io.file_hdf5('/home/rlgraf/Final_Figures/VER_slope_r_1_2_form_location_cut_median_weighted', slope_ver_r_1_2_form_total)
-    ut_io.file_hdf5('/home/rlgraf/Final_Figures/VER_slope_r_4_5_form_location_cut_median_weighted', slope_ver_r_4_5_form_total)
-    ut_io.file_hdf5('/home/rlgraf/Final_Figures/VER_slope_r_7_8_form_location_cut_median_weighted', slope_ver_r_7_8_form_total)
+    ut_io.file_hdf5('/home/rlgraf/Final_Figures/VER_slope_r_4_form_location_cut_median_weighted_publish', slope_ver_r_1_2_form_total)
+    ut_io.file_hdf5('/home/rlgraf/Final_Figures/VER_slope_r_8_form_location_cut_median_weighted_publish', slope_ver_r_4_5_form_total)
+    ut_io.file_hdf5('/home/rlgraf/Final_Figures/VER_slope_r_12_form_location_cut_median_weighted_publish', slope_ver_r_7_8_form_total)
     
-vertical_analysis_form()
+    vertical_analysis_z_0()
+    vertical_analysis_form()
