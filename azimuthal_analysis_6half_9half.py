c698741076e9b8ab7ff9ebc052beebdd2f8c4b51
LAPTOP-6K8H7DVQ\Russell <rlgraf@ucdavis.edu>
2023-09-30 14:43:25 -0700

    edit

--- azimuthal_analysis_6half_9half.py
+++ azimuthal_analysis_6half_9half.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#SBATCH --job-name=azimuthal_analysis_7half_8half
+#SBATCH --job-name=azimuthal_analysis_6half_9half
 #SBATCH --partition=high2  # peloton node: 32 cores, 7.8 GB per core, 250 GB total
 ##SBATCH --partition=high2m  # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
 #SBATCH --mem=64G  # need to specify memory if you set the number of tasks (--ntasks) below
@@ -8,7 +8,7 @@
 #SBATCH --ntasks=1  # (MPI) tasks total
 #SBATCH --cpus-per-task=1  # (OpenMP) threads per (MPI) task
 #SBATCH --time=48:00:00
-#SBATCH --output=azimuthal_analysis_7half_8half_%j.txt
+#SBATCH --output=azimuthal_analysis_6half_9half_%j.txt
 #SBATCH --mail-user=rlgraf@ucdavis.edu
 #SBATCH --mail-type=fail
 #SBATCH --mail-type=begin
@@ -106,7 +106,7 @@ def azimuthal_analysis_z_0():
             for a,b in zip(np.arange(0,14),r90):
                 Fe_H_azim_pre = []
                 for a_pre in np.arange(0,1,0.05):
-                    Fe_H_azim_pre.append(Fe_H_agedependent_sd(7.5,8.5,0,3,0,15,0,3,a+a_pre,a+a_pre+0.05,0,360,r,r_form,age,part))  
+                    Fe_H_azim_pre.append(Fe_H_agedependent_sd(6.5,9.5,0,3,0,15,0,3,a+a_pre,a+a_pre+0.05,0,360,r,r_form,age,part))  
                 Fe_H_azim_pre = np.array(Fe_H_azim_pre)
                 Fe_H_azim_pre_mean = np.nanmean(Fe_H_azim_pre,0)
                 Fe_H_azim.append(Fe_H_azim_pre_mean)
@@ -115,63 +115,6 @@ def azimuthal_analysis_z_0():
     Fe_H_azim_total = np.array(Fe_H_azim_total)
     
     
-    ut_io.file_hdf5('/home/rlgraf/Final_Figures/AZIM_profile_z_0_7half_8half_publish', Fe_H_azim_total) 
-    
-# formation
+    ut_io.file_hdf5('/home/rlgraf/Final_Figures/AZIM_profile_z_0_6half_9half_publish', Fe_H_azim_total) 
 
-def Fe_H_agedependent_sd_form(x1,x2,x3,x4,x5,x6,x7,x8,a1,a2,x9,x10,r_form,r,age,part):
-    index = ut.array.get_indices(r_form[:,0], [x1,x2])
-    index2 = ut.array.get_indices(abs(r_form[:,2]), [x3,x4], prior_indices = index)
-    a_form = part['star'].prop('form.scalefactor')
-    scaled_radius = r_form[:,0]/a_form
-    index3 = ut.array.get_indices(scaled_radius,[x5,x6], prior_indices = index2)
-    index4 = ut.array.get_indices(abs(r[:,2]), [x7,x8], prior_indices = index3)
-    index5 = ut.array.get_indices(age, [a1,a2], prior_indices = index4)
-    index6 = ut.array.get_indices(r[:,1]*180/np.pi, [x9,x10], prior_indices = index5)
-    Fe_H = part['star'].prop('metallicity.iron')
-    Fe_H_cut = Fe_H[index6]
-    if len(Fe_H_cut) == 0:
-        return(np.nan)
-    sd = np.std(Fe_H_cut)
-    return(sd)
-
-def azimuthal_analysis_form():
-    
-    Fe_H_azim_form_total = []
-    slope_azim_form_total = []
-    sim = sim_func()
-    R90 = R90_func()
-    LG_counter = 0
-    for q,s in enumerate(sim):
-        simulation_directory = s
-        part = gizmo.io.Read.read_snapshots(['star'], 'redshift', 0, simulation_directory, assign_hosts_rotation=True, assign_formation_coordinates = True)
-        Fe_H = part['star'].prop('metallicity.iron')
-        age = part['star'].prop('age')
-        
-        if s in ['/group/awetzelgrp/m12_elvis/m12_elvis_RomeoJuliet_r3500', '/group/awetzelgrp/m12_elvis/m12_elvis_RomulusRemus_r4000', '/group/awetzelgrp/m12_elvis/m12_elvis_ThelmaLouise_r4000']:
-            r_array = [part['star'].prop('host1.distance.principal.cylindrical'), part['star'].prop('host2.distance.principal.cylindrical')]
-            r_form_array = [part['star'].prop('form.host1.distance.principal.cylindrical'), part['star'].prop('form.host2.distance.principal.cylindrical')]
-        else:
-            r_array = [part['star'].prop('host.distance.principal.cylindrical')]
-            r_form_array = [part['star'].prop('form.host.distance.principal.cylindrical')]
-            
-        for c, (r, r_form) in enumerate(zip(r_array, r_form_array)):
-            Fe_H_azim_form = []
-            slope_azim_form = []
-            LG_counter += c
-            r90 = R90[q+LG_counter]
-            for a_f,b_f in zip(np.arange(0,14), r90):
-                Fe_H_azim_pre_f = []
-                for a_f_pre in np.arange(0,1,0.05):
-                      Fe_H_azim_pre_f.append(Fe_H_agedependent_sd_form(7.5,8.5,0,3,0,30,0,3,a_f+a_f_pre,a_f+a_f_pre+0.05,0,360,r_form,r,age,part))
-                Fe_H_azim_pre_f = np.array(Fe_H_azim_pre_f)
-                Fe_H_azim_pre_mean_f = np.nanmean(Fe_H_azim_pre_f,0)
-                Fe_H_azim_form.append(Fe_H_azim_pre_mean_f)
-    
-            Fe_H_azim_form_total.append(Fe_H_azim_form)
-    Fe_H_azim_form_total = np.array(Fe_H_azim_form_total)
-    
-    ut_io.file_hdf5('/home/rlgraf/Final_Figures/AZIM_profile_form_7hafl_8half_publish', Fe_H_azim_form_total) 
-    
 azimuthal_analysis_z_0()
-azimuthal_analysis_form()
