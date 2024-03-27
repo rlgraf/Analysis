39b4ba5499f09619dc04bbfc795302bee2e1c351
LAPTOP-6K8H7DVQ\Russell <rlgraf@ucdavis.edu>
2024-03-26 17:56:44 -0700

    edit

---
+++ host_age_test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+#SBATCH --job-name=host_age_test
+#SBATCH --partition=high2  # peloton node: 32 cores, 7.8 GB per core, 250 GB total
+##SBATCH --partition=high2m  # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
+#SBATCH --mem=32G  # need to specify memory if you set the number of tasks (--ntasks) below
+##SBATCH --nodes=1  # if you specify this, the number of nodes, do not set memory (--mem) above
+##SBATCH --ntasks-per-node=1  # (MPI) tasks per node
+#SBATCH --ntasks=1  # (MPI) tasks total
+#SBATCH --cpus-per-task=1  # (OpenMP) threads per (MPI) task
+#SBATCH --time=08:00:00
+#SBATCH --output=host_age_test_%j.txt
+#SBATCH --mail-user=rlgraf@ucdavis.edu
+#SBATCH --mail-type=fail
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=end
+import os
+import utilities.io as ut_io
+# print run-time and CPU information
+ScriptPrint = ut_io.SubmissionScriptClass("slurm")
+# Analysis code
+# Import programs
+#pip install weightedstats==0.4.1
+#conda install -c conda-forge weightedstats
+import numpy as np
+import gizmo_analysis as gizmo
+import utilities as ut
+import scipy
+import utilities.io as ut_io
+import weightedstats as ws
+
+
+simulation_directory = '/group/awetzelgrp/m12i/m12i_r7100_uvb-late/'
+part = gizmo.io.Read.read_snapshots(['star'], 'redshift', 0, simulation_directory, assign_hosts_rotation=True, assign_formation_coordinates = True)
+
+age1 = part['star'].prop('host1.age')
+print(age1)
+
+age2 = part['star'].prop('host2.age')
+print(age2)
+
+    
+    
