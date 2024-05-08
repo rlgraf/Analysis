c037737aaa7a729f2cd39b84f5ff6904012f90c6
LAPTOP-6K8H7DVQ\Russell <rlgraf@ucdavis.edu>
2024-05-08 15:32:32 -0700

    edit

--- half_mass_radius_analysis_percentile_form_young.py
+++ half_mass_radius_analysis_percentile_form_young.py
@@ -63,8 +63,8 @@ def half_mass_radius_form(x3,x4,a1,a2,r,r_form,age,part):
     #index1 = ut.array.get_indices(abs(r_form[:,2]), [x1,x2])
     a_form = part['star'].prop('form.scalefactor')
     scaled_radius = r_form[:,0]/a_form
-    index2 = ut.array.get_indices(scaled_radius, [x3,x4])
-    index3 = ut.array.get_indices(age, [a1,a2], prior_indices = index2)
+    index2 = ut.array.get_indices(age, [a1,a2])
+    index3 = ut.array.get_indices(scaled_radius, [x3,x4], prior_indices = index2)
     mass_cut = ut.math.percentile_weighted(r_form[:,0][index3], 90, weights = part['star']['mass'][index3])
     
     return(mass_cut)
