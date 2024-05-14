4ec69effacea7ba9b1868e1de51ad02541ff1492
LAPTOP-6K8H7DVQ\Russell <rlgraf@ucdavis.edu>
2024-05-14 15:50:05 -0700

    edit

--- half_mass_radius_analysis_percentile_form_spherical.py
+++ half_mass_radius_analysis_percentile_form_spherical.py
@@ -65,7 +65,7 @@ def half_mass_radius_form(x1,x2,x3,x4,a1,a2,r,r_form,age,part):
     scaled_radius = r_form[:,0]/a_form
     index2 = ut.array.get_indices(scaled_radius, [x3,x4])
     index3 = ut.array.get_indices(age, [a1,a2], prior_indices = index2)
-    if len(r_form[:,0][index3] == 0:
+    if len(r_form[:,0][index3]) == 0:
         return(np.nan)
     else:
         mass_cut = ut.math.percentile_weighted(r_form[:,0][index3], 50, weights = part['star']['mass'][index3])
