a9ea76db22c2a2a51c12c2f9667079ee7fb8b7bd
LAPTOP-6K8H7DVQ\Russell <rlgraf@ucdavis.edu>
2024-06-22 15:00:38 -0700

    edit

--- radial_analysis_surface_density_ratio_and_Fe_H_100Myr_R90_young.py
+++ radial_analysis_surface_density_ratio_and_Fe_H_100Myr_R90_young.py
@@ -7,7 +7,7 @@
 ##SBATCH --ntasks-per-node=1  # (MPI) tasks per node
 #SBATCH --ntasks=1  # (MPI) tasks total
 #SBATCH --cpus-per-task=1  # (OpenMP) threads per (MPI) task
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --output=radial_analysis_surface_density_ratio_and_Fe_H_100Myr_R90_young_%j.txt
 #SBATCH --mail-user=rlgraf@ucdavis.edu
 #SBATCH --mail-type=fail
