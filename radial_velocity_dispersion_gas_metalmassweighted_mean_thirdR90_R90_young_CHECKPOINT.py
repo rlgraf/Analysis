4d4fb45c128987988aab91b0e55e80d8a17e8d34
LAPTOP-6K8H7DVQ\Russell <rlgraf@ucdavis.edu>
2024-07-02 17:07:34 -0700

    edit

--- radial_velocity_dispersion_gas_metalmassweighted_mean_thirdR90_R90_young_CHECKPOINT.py
+++ radial_velocity_dispersion_gas_metalmassweighted_mean_thirdR90_R90_young_CHECKPOINT.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#SBATCH --job-name=radial_velocity_dispersion_gas_metalmassweighted_mean_thirdR90_R90_CHECKPOINT
+#SBATCH --job-name=radial_velocity_dispersion_gas_metalmassweighted_mean_thirdR90_R90_young_CHECKPOINT
 #SBATCH --partition=high2  # peloton node: 32 cores, 7.8 GB per core, 250 GB total
 ##SBATCH --partition=high2m  # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
 #SBATCH --mem=99G  # need to specify memory if you set the number of tasks (--ntasks) below
@@ -8,7 +8,7 @@
 #SBATCH --ntasks=1  # (MPI) tasks total
 #SBATCH --cpus-per-task=1  # (OpenMP) threads per (MPI) task
 #SBATCH --time=40:00:00
-#SBATCH --output=radial_velocity_dispersion_gas_metalmassweighted_mean_thirdR90_R90_CHECKPOINT_%j.txt
+#SBATCH --output=radial_velocity_dispersion_gas_metalmassweighted_mean_thirdR90_R90_young_CHECKPOINT_%j.txt
 #SBATCH --mail-user=rlgraf@ucdavis.edu
 #SBATCH --mail-type=fail
 #SBATCH --mail-type=begin
@@ -254,6 +254,6 @@ def radial_vel_disp_gas():
     
     radial_vel_disp_gas_all_galaxies = np.array(radial_vel_disp_gas_all_galaxies)
            
-    ut_io.file_hdf5('/home/rlgraf/Final_Figures/velocity_dispersion_gas_metalmassweighted_mean_thirdR90_R90_CHECKPOINT', radial_vel_disp_gas_all_galaxies)
+    ut_io.file_hdf5('/home/rlgraf/Final_Figures/velocity_dispersion_gas_metalmassweighted_mean_thirdR90_R90_young_CHECKPOINT', radial_vel_disp_gas_all_galaxies)
 
 radial_vel_disp_gas()
