1e5f738eda525b1024ff33daa908cb46cb91a7d7
LAPTOP-6K8H7DVQ\Russell <rlgraf@ucdavis.edu>
2024-08-16 22:09:25 -0700

    edit

--- radial_mean_velocity_gas_diff_redshifts_metal_multisnapavg_m12i_m12c_m12f.py
+++ radial_mean_velocity_gas_diff_redshifts_metal_multisnapavg_m12i_m12c_m12f.py
@@ -36,6 +36,8 @@ def velocity_dispersion_gas(x1,x2,x3,x4,r,v,part):
     index2 = ut.array.get_indices(abs(r[:,2]), [x3,x4], prior_indices = index)
     vel_rad = v[:,0]
     massfrac_iron = part['gas']['massfraction'][:, 10]
+    if len(vel_rad[index2]) == 0:
+        return(np.nan)
     return(np.average(vel_rad[index2], weights = massfrac_iron[index2]*part['gas']['mass'][index2]))
            
 def radial_vel_disp_gas():
